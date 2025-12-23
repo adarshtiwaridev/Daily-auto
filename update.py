c69a8283fec6ff717d1cef63cdb159df72460c46
adarsh2027dev <adarshtiwaridev01@gmail.com>
2025-12-23 20:45:38 +0530

    add random night commit timing

--- update.py
+++ update.py
@@ -1,6 +1,14 @@
+import time
+import random
 from datetime import datetime
 
-with open("log.txt", "a") as f:
-    f.write(f"update at {datetime.now()}\n")
+# random delay between 0 to 40 minutes (in seconds)
+delay = random.randint(0, 40 * 60)
 
-print("Updated successfully")
+print(f"Waiting for {delay//60} minutes before commit...")
+time.sleep(delay)
+
+with open("log.txt", "a") as f:
+    f.write(f"Auto commit at {datetime.now()}\n")
+
+print("Commit file updated")
